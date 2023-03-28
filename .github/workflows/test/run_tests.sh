54cc10bddab55cd75982a1014c5b08cf54b6b39b
ChrisN <kris_nich@hotmail.co.uk>
2023-03-28 18:52:48 +0100

    gitaction testing

--- .github/workflows/test/run_tests.sh
+++ .github/workflows/test/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 pip install --user pytest && \
 echo Executing API Tests... && \
-pytest -p no:warnings -v --junitxml=/opt/test_results/pytest.xml /usr/src/app/tests/api
+pytest -p no:warnings -v /usr/src/app/tests/api
 # echo Executing Processor Tests... && \
-# pytest -p no:warnings -v --junitxml=/opt/test_results/pytest.xml /usr/src/app/tests/dataproc
+# pytest -p no:warnings -v /usr/src/app/tests/dataproc
