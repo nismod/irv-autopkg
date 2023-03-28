d855e5142596f24e746e76db48f813ec828d843c
ChrisN <kris_nich@hotmail.co.uk>
2023-03-28 20:42:21 +0100

    add processor tests to action

--- .github/workflows/test/run_tests.sh
+++ .github/workflows/test/run_tests.sh
@@ -2,6 +2,6 @@
 pip install pytest && \
 echo Executing API Tests... && \
 ls -lrt /usr/src/app/tests/data && \
-pytest -p no:warnings -v /usr/src/app/tests/api
-# echo Executing Processor Tests... && \
-# pytest -p no:warnings -v /usr/src/app/tests/dataproc
+pytest -p no:warnings -v /usr/src/app/tests/api && \
+echo Executing Processor Tests... && \
+pytest -p no:warnings -v /usr/src/app/tests/dataproc
